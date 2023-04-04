1cb30048e4ad3183260b0793a496905e0aa90f1c
Narayana_Reddy <narayanachavva@gmail.com>
2023-04-05 01:09:30 +0530

    optmized code

--- src/Controllers/imageControllers.js
+++ src/Controllers/imageControllers.js
@@ -1,45 +1,51 @@
-let imageModel=require("../Model/imageSchemas")
-let Model= require("../Model/imageSchemas")
-let getImage = async (req, res) => {
-    try {
-        let data = await imageModel.find();
-        res.status(201).json({
-            status: "Success",
-            data: data
-        });
-    } catch (e) {
-        res.status(404).json({
-            status: "Failed",
-            message: e.message,
-        });
-    }
-}
+const ImageModel = require("../Model/imageSchemas");
 
-let postImage=(req, res) => {
-    const { filename } = req.file;
-    const saveImage =  Model({
-        name: req.body.name,
-        description: req.body.description,
-        location: req.body.location,
-        date: new Date().toLocaleDateString(),
-        likes: parseInt(Math.random() * 100),
-        photo: filename
+const getImage = async (req, res) => {
+  try {
+    const data = await ImageModel.find();
+    res.status(200).json({
+      status: "Success",
+      data: data,
     });
-    saveImage
-      .save()
-      res.send("data uploaded succefully")
+  } catch (e) {
+    res.status(404).json({
+      status: "Failed",
+      message: e.message,
+    });
   }
+};
 
-let deleteImage= async (req, res) => {
-    try {
-      let data = await imageModel.deleteOne({ _id: req.params.id });
-      res.send(data);
-    } catch (e) {
-      res.status(404).json({
-        status: "Failed",
-        message: e.message,
-      });
-    }
+const postImage = async (req, res) => {
+  const { filename } = req.file;
+  const saveImage = new ImageModel({
+    name: req.body.name,
+    description: req.body.description,
+    location: req.body.location,
+    date: new Date().toLocaleDateString(),
+    likes: parseInt(Math.random() * 100),
+    photo: filename,
+  });
+  try {
+    await saveImage.save();
+    res.status(201).send("Data uploaded successfully");
+  } catch (e) {
+    res.status(404).json({
+      status: "Failed",
+      message: e.message,
+    });
   }
+};
 
-  module.exports={deleteImage,getImage,postImage }
+const deleteImage = async (req, res) => {
+  try {
+    const data = await ImageModel.deleteOne({ _id: req.params.id });
+    res.send(data);
+  } catch (e) {
+    res.status(404).json({
+      status: "Failed",
+      message: e.message,
+    });
+  }
+};
+
+module.exports = { deleteImage, getImage, postImage };
